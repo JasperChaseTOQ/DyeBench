7e7a2ae83c2a327624ec4a25d7d5df3ffe6772bd
JasperChaseTOQ <103849596+JasperChaseTOQ@users.noreply.github.com>
2022-07-01 16:26:16 -0400

    Update DyeBenchItemsSetup.java

--- src/main/java/me/jasperchasetoq/dyebench/setup/DyeBenchItemsSetup.java
+++ src/main/java/me/jasperchasetoq/dyebench/setup/DyeBenchItemsSetup.java
@@ -25,14 +25,103 @@ import javax.annotation.Nonnull;
                             new ItemStack(Material.WHITE_BED, 1), new ItemStack(Material.LOOM, 1), new ItemStack(Material.WHITE_CANDLE, 1),
                             new ItemStack(Material.WHITE_SHULKER_BOX, 1), new ItemStack(Material.WHITE_CONCRETE, 1), new ItemStack(Material.WHITE_STAINED_GLASS, 1),
                     }).register(plugin);
+            //pigments
             new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_WHITE_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
                     new ItemStack[]{
                             new ItemStack(Material.WHITE_DYE, 64), new ItemStack(Material.WHITE_DYE, 64), new ItemStack(Material.WHITE_DYE, 64),
                             new ItemStack(Material.WHITE_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.WHITE_DYE, 64),
                             new ItemStack(Material.WHITE_DYE, 64), new ItemStack(Material.WHITE_DYE, 64), new ItemStack(Material.WHITE_DYE, 64),
                     }).register(plugin);
-            //pigments
-
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_ORANGE_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.ORANGE_DYE, 64), new ItemStack(Material.ORANGE_DYE, 64), new ItemStack(Material.ORANGE_DYE, 64),
+                            new ItemStack(Material.ORANGE_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.ORANGE_DYE, 64),
+                            new ItemStack(Material.ORANGE_DYE, 64), new ItemStack(Material.ORANGE_DYE, 64), new ItemStack(Material.ORANGE_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_MAGENTA_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.MAGENTA_DYE, 64), new ItemStack(Material.MAGENTA_DYE, 64), new ItemStack(Material.MAGENTA_DYE, 64),
+                            new ItemStack(Material.MAGENTA_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.MAGENTA_DYE, 64),
+                            new ItemStack(Material.MAGENTA_DYE, 64), new ItemStack(Material.MAGENTA_DYE, 64), new ItemStack(Material.MAGENTA_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_LIGHT_BLUE_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.LIGHT_BLUE_DYE, 64), new ItemStack(Material.LIGHT_BLUE_DYE, 64), new ItemStack(Material.LIGHT_BLUE_DYE, 64),
+                            new ItemStack(Material.LIGHT_BLUE_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.LIGHT_BLUE_DYE, 64),
+                            new ItemStack(Material.LIGHT_BLUE_DYE, 64), new ItemStack(Material.LIGHT_BLUE_DYE, 64), new ItemStack(Material.LIGHT_BLUE_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_YELLOW_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.YELLOW_DYE, 64), new ItemStack(Material.YELLOW_DYE, 64), new ItemStack(Material.YELLOW_DYE, 64),
+                            new ItemStack(Material.YELLOW_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.YELLOW_DYE, 64),
+                            new ItemStack(Material.YELLOW_DYE, 64), new ItemStack(Material.YELLOW_DYE, 64), new ItemStack(Material.YELLOW_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_LIME_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.LIME_DYE, 64), new ItemStack(Material.LIME_DYE, 64), new ItemStack(Material.LIME_DYE, 64),
+                            new ItemStack(Material.LIME_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.LIME_DYE, 64),
+                            new ItemStack(Material.LIME_DYE, 64), new ItemStack(Material.LIME_DYE, 64), new ItemStack(Material.LIME_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_PINK_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.PINK_DYE, 64), new ItemStack(Material.PINK_DYE, 64), new ItemStack(Material.PINK_DYE, 64),
+                            new ItemStack(Material.PINK_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.PINK_DYE, 64),
+                            new ItemStack(Material.PINK_DYE, 64), new ItemStack(Material.PINK_DYE, 64), new ItemStack(Material.PINK_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_GRAY_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.GRAY_DYE, 64), new ItemStack(Material.GRAY_DYE, 64), new ItemStack(Material.GRAY_DYE, 64),
+                            new ItemStack(Material.GRAY_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.GRAY_DYE, 64),
+                            new ItemStack(Material.GRAY_DYE, 64), new ItemStack(Material.GRAY_DYE, 64), new ItemStack(Material.GRAY_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_LIGHT_GRAY_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.LIGHT_GRAY_DYE, 64), new ItemStack(Material.LIGHT_GRAY_DYE, 64), new ItemStack(Material.LIGHT_GRAY_DYE, 64),
+                            new ItemStack(Material.LIGHT_GRAY_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.LIGHT_GRAY_DYE, 64),
+                            new ItemStack(Material.LIGHT_GRAY_DYE, 64), new ItemStack(Material.LIGHT_GRAY_DYE, 64), new ItemStack(Material.LIGHT_GRAY_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_CYAN_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.CYAN_DYE, 64), new ItemStack(Material.CYAN_DYE, 64), new ItemStack(Material.CYAN_DYE, 64),
+                            new ItemStack(Material.CYAN_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.CYAN_DYE, 64),
+                            new ItemStack(Material.CYAN_DYE, 64), new ItemStack(Material.CYAN_DYE, 64), new ItemStack(Material.CYAN_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_PURPLE_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.PURPLE_DYE, 64), new ItemStack(Material.PURPLE_DYE, 64), new ItemStack(Material.PURPLE_DYE, 64),
+                            new ItemStack(Material.PURPLE_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.PURPLE_DYE, 64),
+                            new ItemStack(Material.PURPLE_DYE, 64), new ItemStack(Material.PURPLE_DYE, 64), new ItemStack(Material.PURPLE_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_BLUE_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.BLUE_DYE, 64), new ItemStack(Material.BLUE_DYE, 64), new ItemStack(Material.BLUE_DYE, 64),
+                            new ItemStack(Material.BLUE_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.BLUE_DYE, 64),
+                            new ItemStack(Material.BLUE_DYE, 64), new ItemStack(Material.BLUE_DYE, 64), new ItemStack(Material.BLUE_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_BROWN_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.BROWN_DYE, 64), new ItemStack(Material.BROWN_DYE, 64), new ItemStack(Material.BROWN_DYE, 64),
+                            new ItemStack(Material.BROWN_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.BROWN_DYE, 64),
+                            new ItemStack(Material.BROWN_DYE, 64), new ItemStack(Material.BROWN_DYE, 64), new ItemStack(Material.BROWN_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_GREEN_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.GREEN_DYE, 64), new ItemStack(Material.GREEN_DYE, 64), new ItemStack(Material.GREEN_DYE, 64),
+                            new ItemStack(Material.GREEN_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.GREEN_DYE, 64),
+                            new ItemStack(Material.GREEN_DYE, 64), new ItemStack(Material.GREEN_DYE, 64), new ItemStack(Material.GREEN_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_RED_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.RED_DYE, 64), new ItemStack(Material.RED_DYE, 64), new ItemStack(Material.RED_DYE, 64),
+                            new ItemStack(Material.RED_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.RED_DYE, 64),
+                            new ItemStack(Material.RED_DYE, 64), new ItemStack(Material.RED_DYE, 64), new ItemStack(Material.RED_DYE, 64),
+                    }).register(plugin);
+            new Pigments(DyebenchItems.DB_PIGMENTS, DyebenchItems.DB_BLACK_PIGMENT, RecipeType.ENHANCED_CRAFTING_TABLE,
+                    new ItemStack[]{
+                            new ItemStack(Material.BLACK_DYE, 64), new ItemStack(Material.BLACK_DYE, 64), new ItemStack(Material.BLACK_DYE, 64),
+                            new ItemStack(Material.BLACK_DYE, 64), new ItemStack(Material.LOOM, 1), new ItemStack(Material.BLACK_DYE, 64),
+                            new ItemStack(Material.BLACK_DYE, 64), new ItemStack(Material.BLACK_DYE, 64), new ItemStack(Material.BLACK_DYE, 64),
+                    }).register(plugin);
             //Dyeable Blocks
             new SlimefunItem(DyebenchItems.DB_DYEABLE_BLOCKS, DyebenchItems.DB_DYEABLE_WOOL, RecipeType.NULL,
                     new ItemStack[]{null, null, null, null, DyebenchItems.DB_DYEABLE_WOOL, null, null, null, null,}).register(plugin);
